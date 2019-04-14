645eff58c471222ac44f8107ce6f731f66127269
Prasob kumar kp <prasobkumarkp@gmail.com>
2019-04-14 16:37:42 +0530

    validation added for selected days.

---
+++ EventRegistration/Attributes/CheckBoxValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using EventRegistration.Models;
+
+namespace EventRegistration.Attributes
+{
+    public class CheckBoxValidator : ValidationAttribute
+    {
+        public object MinimumNumberOptionShouldBeSelected { get; set; }
+ 
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (Convert.ToInt32(MinimumNumberOptionShouldBeSelected) <= (value as List<ChecklistItem>)?.Select(s=>s.Selected)?.Count())
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult($"At least {MinimumNumberOptionShouldBeSelected} should be selected");
+            }
+        }
+    }
+}
--- EventRegistration/ViewModels/AddEventViewModel.cs
+++ EventRegistration/ViewModels/AddEventViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using EventRegistration.Models;
 using System.Collections.Generic;
+using EventRegistration.Attributes;
 
 namespace EventRegistration.ViewModels
 {
     public class AddEventViewModel
     {
         public Registration Registration { get; set; }
+
+        [CheckBoxValidator(MinimumNumberOptionShouldBeSelected = 1)]
         public List<ChecklistItem> EventDays { get; set; }
+
         public List<ChecklistItem> Genders { get; set; }
     }
 }
